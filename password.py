af206a4ea7f3555eed20b551bc7d8d1977780188
Tony <158815401+frogCaller@users.noreply.github.com>
2024-06-21 21:38:40 -0700

    Update password.py

--- password.py
+++ password.py
@@ -4,25 +4,17 @@
 
 
 import time
-import random
-import sys
-sys.path.append('/home/pi/python/screen/drive')
-import os
-import requests
+import datetime
 from datetime import datetime
-
-libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'drive')
-if os.path.exists(libdir):
-    sys.path.append(libdir)
-import spidev
+import sys
+import os
+import pygame
 import logging
+from PIL import Image, ImageDraw, ImageFont
 from drive import SSD1305
-
-from PIL import Image,ImageDraw,ImageFont
-
+import random
 import subprocess
 
-# 128x32 display with hardware SPI:
 disp = SSD1305.SSD1305()
 
 disp.Init()
@@ -32,30 +24,21 @@ logging.info("clear display")
 disp.clear()
 
 # Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
-# Get drawing object to draw on image.
 draw = ImageDraw.Draw(image)
 
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
 # Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
 padding = 0
 top = padding
 bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
 timeframe = 0.01
 # Load default font.
-#font = ImageFont.load_default()
-
-# Alternatively load a TTF font.  Make sure the .ttf font file is in the same directory as the python script!
-# Some other nice fonts to try: http://www.dafont.com/bitmap.php
 font = ImageFont.truetype('04B_08__.TTF',8)
 
 lower = "abcdefghijklmnopqrstuvwxyz"
@@ -91,20 +74,20 @@ while True:
         current_date = datetime.now()
         # Draw a black filled box to clear the image.
         draw.rectangle((0,0,width,height), outline=0, fill=0)
-        
+
         #Date & Time
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
-        linetext(8, str(current_date.strftime("%A") + "         " + str(current_date.strftime("%-I:%M %p"))))
-        linetext(24, str(current_date.strftime("%B %d %Y")))
+        linetext(0, str(current_date.strftime("%A") + "         " + str(current_date.strftime("%-I:%M %p"))))
+        linetext(16, str(current_date.strftime("%B %d %Y")))
 
         buffer(3)
 
-        for m in range(500): 
+        for m in range(500):
             linetext(0, "Rockyou.txt")
-            linetext(16, "" + pick_random_line("/home/pi/python/rockyou.txt"))
+            linetext(16, "" + pick_random_line("rockyou.txt"))
             buffer(0.025)
 
-        for m in range(500):  
+        for m in range(500):
             linetext(0, "Brute Force")
             linetext(16, "" + passwordguess(15))
             buffer(0.025)
